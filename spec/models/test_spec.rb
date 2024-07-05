e6535a028a88b190b7b47bdf2bc58a12bd0687d6
Billy Wallace <wallace.billy1@gmail.com>
2024-07-05 15:14:56 -0600

    Update fake test file to have one passing and one failing test

--- spec/models/test_spec.rb
+++ spec/models/test_spec.rb
@@ -1,6 +1,11 @@
 RSpec.describe "Test" do
   describe '#test_name' do
-    it 'confirms that CI works' do
+    it 'confirms that CI works - bad tests' do
+      expect(2+2).to eq("banana")
+      expect("string").to be_a(Array)
+    end
+
+    it 'confirms that CI works - good tests' do
       expect(2+2).to eq(4)
       expect("string").to be_a(String)
     end
