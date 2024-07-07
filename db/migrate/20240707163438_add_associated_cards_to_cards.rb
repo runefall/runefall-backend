39ab505352a390dc302d67c040dd755440ad6a3f
Jared Hobson <jaredmhobson@gmail.com>
2024-07-07 09:40:55 -0700

    Run migration to add associated_cards column to cards

--- db/migrate/20240707163438_add_associated_cards_to_cards.rb
+++ db/migrate/20240707163438_add_associated_cards_to_cards.rb
@@ -1,5 +1,5 @@
 class AddAssociatedCardsToCards < ActiveRecord::Migration[7.1]
   def change
-    add_column :cards, :associated_cards, array: true, default: []
+    add_column :cards, :associated_cards, :string, array: true, default: []
   end
 end
