8f6e403fd366fba5b934691ca0f93277dda5c9df
Jared Hobson <jaredmhobson@gmail.com>
2024-07-07 03:03:04 -0700

    Add card serializer

---
+++ app/serializers/card_serializer.rb
@@ -0,0 +1,28 @@
+class CardSerializer
+  include JSONAPI::Serializer
+  attributes :name,
+             :card_code,
+             :description,
+             :description_raw,
+             :levelup_description,
+             :levelup_description_raw,
+             :flavor_text,
+             :artist_name,
+             :attack,
+             :cost,
+             :health,
+             :spell_speed,
+             :rarity,
+             :supertype,
+             :card_type,
+             :collectible,
+             :set,
+             :associated_card_refs,
+             :regions,
+             :region_refs,
+             :keywords,
+             :keyword_refs,
+             :formats,
+             :format_refs,
+             :assets
+end
