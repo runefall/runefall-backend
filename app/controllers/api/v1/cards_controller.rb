0da790b09ce608cadf023899fc15d85895fd3194
Billy Wallace <wallace.billy1@gmail.com>
2024-07-07 16:07:02 -0600

    Create search controller action and associate route and model AR query method

--- app/controllers/api/v1/cards_controller.rb
+++ app/controllers/api/v1/cards_controller.rb
@@ -13,4 +13,37 @@ class Api::V1::CardsController < ApplicationController
     cards = Card.all
     render json: CardSerializer.new(cards)
   end
+
+  def search
+    if valid_search_params?
+      cards = Card.search(format_search_params)
+      render json: CardSerializer.new(cards)
+    else
+      render json: { error: "Invalid search query" }, status: :bad_request
+    end
+  end
+
+  private
+  def format_search_params
+    query_params = params[:query].split(" ")
+    hash = { }
+    query_params = query_params.each do |string|
+      if string.include?(":")
+        filter_array = string.split(":")
+        hash[filter_array[0].to_sym] = filter_array[1]
+      else 
+        hash[:name] = string
+      end
+    end
+    hash
+  end
+
+  def valid_search_params?
+    keys = format_search_params.keys
+    keys.all? { |key| permitted_search_criteria.include?(key) }
+  end
+
+  def permitted_search_criteria
+    [:name, :description]
+  end
 end
